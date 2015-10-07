7d4151ca29541456725901ce3449deff0c8b6d93
Sahil <sahil@unicommerce.com>
2015-10-07 09:12:27 +0530

    annam revamp 1st commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+    //Menu Toggling Script
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+
+    $("#owl-demo").owlCarousel({
+        navigation : true,
+        items : 1,
+        navigation:false,
+        autoPlay:true
+    });
+
+    $('input,textarea').blur(function() {
+        var $this = $(this);
+        if ($this.val())
+            $this.addClass('used');
+        else
+            $this.removeClass('used');
+    });
+
+    var $ripples = $('.ripples');
+
+    $ripples.on('click.Ripples', function(e) {
+
+        var $this = $(this);
+        var $offset = $this.parent().offset();
+        var $circle = $this.find('.ripplesCircle');
+
+        var x = e.pageX - $offset.left;
+        var y = e.pageY - $offset.top;
+
+        $circle.css({
+            top: y + 'px',
+            left: x + 'px'
+        });
+
+        $this.addClass('is-active');
+
+    });
+
+    $ripples.on('animationend webkitAnimationEnd mozAnimationEnd oanimationend MSAnimationEnd', function(e) {
+        $(this).removeClass('is-active');
+    });
+
+});
